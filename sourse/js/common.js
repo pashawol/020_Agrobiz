3feb9f4fcc7405236fc5d1e2f933f19149f268e4
Roman Makeev <elvizlir@gmail.com>
2019-08-29 02:14:04 +0300

    delete some trash

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -9,35 +9,18 @@ jQuery(document).ready(function ($) {
 	document.createElement("picture");
 	// для свг
 	svg4everybody({}); 
-	JSCCommon.magnificPopupCall();
-
-	JSCCommon.tabscostume('tabs');
-
-	JSCCommon.mobileMenu();
-
-	JSCCommon.inputMask(); 
-
-	JSCCommon.CustomInputFile(); 
-	// добавляет подложку для pixel perfect
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/main.jpg);"></div>')
-	// /добавляет подложку для pixel perfect
-
 
 	$(".header-block__btn").on("click", function (event) {
-		event.preventDefault(); //опустошим стандартную обработку
-		var id  = $(this).attr('href'), //заберем айдишник блока с параметром URL
-			top = $(id).offset().top; //определим высоту от начала страницы до якоря
-	$('body,html').animate({scrollTop: top}, 1000); //сделаем прокрутку за 1 с
-	});
-
-
-	$(window).resize(function () {
-		heightses();
-
+		event.preventDefault();
+		var id  = $(this).attr('href'), 
+			top = $(id).offset().top; 
+	$('body,html').animate({scrollTop: top}, 1000); 
 	});
  
-	heightses();
  
+
+
+
 
 	// form
 	$("form").submit(function () { //Change
@@ -66,83 +49,4 @@ jQuery(document).ready(function ($) {
 	});
 	// /form
 });
-JSCCommon = {
-	// часть вызов скриптов здесь, для использования при AJAX
-	// функции для запуска lazy
-	 
-
-	// /LazyFunction
-
-	magnificPopupCall: function () {
-		$('.popup-with-move-anim').magnificPopup({
-			type: 'inline',
-
-			fixedContentPos: true,
-			fixedBgPos: true,
-
-			overflowY: 'auto',
-
-			closeBtnInside: true,
-			preloader: false,
-
-			midClick: true,
-			removalDelay: 300,
-			mainClass: 'my-mfp-zoom-in',
-			tClose: 'Закрыть (Esc)',
-		});
-
-		// / modal window
-
-		// modal галерея
-		$(".gal").each(function () {
-
-			$(this).find("a").magnificPopup({
-				type: 'image',
-				closeOnContentClick: false,
-				closeBtnInside: false,
-				mainClass: 'mfp-with-zoom mfp-img-mobile',
-				tClose: 'Закрыть (Esc)',
-				image: {
-					verticalFit: true,
-					// titleSrc: function(item) {
-					//   return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-					// }
-
-				},
-				gallery: {
-					enabled: true,
-					tPrev: 'Назад (Кнопка влева)', // title for left button
-					tNext: 'Вперед (Кнопка вправа)', // title for right button
-					tCounter: '<span class="mfp-counter">%curr% из %total%</span>',
-				}
-			});
-		})
-
-	// табы  . 
-	tabscostume: function (tab) {
-		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
-			$(this)
-				.addClass('active').siblings().removeClass('active')
-				.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-				.eq($(this).index()).fadeIn().addClass('active');
-
-		});
-	},
-	// /табы  . 
-
- 
-	// /nlineSVG
-	CustomInputFile: function CustomInputFile() {
-		var file = $(".add-file input[type=file]");
-		file.change(function () {
-			var filename = $(this).val().replace(/.*\\/, "");
-			var name = $(".add-file__filename  ");
-			name.text(filename);
-
-		});
-	},
-
-
-// JSCCommon.LazyFunction();
-/***/
 
